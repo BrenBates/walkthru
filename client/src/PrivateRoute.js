fbdd5ee2517aab33837b5ae80683b934127652ae
colinSeven8 <ccnorse80@gmail.com>
2020-01-20 21:40:39 -0700

    blah

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,22 +1,22 @@
 import React from "react";
-import axios from 'axios';
+//import axios from 'axios';
 import { Route, Redirect } from "react-router-dom";
 import { useAuth } from "./context/auth";
 
 function PrivateRoute({ component: Component, ...rest }) {
   const { authTokens } = useAuth();
   
-  const validateToken = token => {
+  // const validateToken = token => {
 
-    axios.get("/api/users/validatetoken", token)
+  //   axios.get("/api/users/validatetoken", token)
     
-    console.log(token)
-    if(token) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  //   console.log(token)
+  //   if(token) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
 
   return (
     <Route
