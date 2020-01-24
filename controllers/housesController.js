4bf091052e8c60ee9c03322656ebe88796266d82
colinSeven8 <ccnorse80@gmail.com>
2020-01-24 14:51:53 -0700

    Testing some Formik

--- controllers/housesController.js
+++ controllers/housesController.js
@@ -1,7 +1,12 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the housesController
 module.exports = {
+  // House.findOne().populate('comments').exec(function(error, user) {
+  //   for (let i = 0; i < user.comments.length; i++) {
+  //     const element = user.comments[i];
+  //     console.log('element ', element);    }
+  // });
   findAll: function(req, res) {
     console.log('findAll');
     db.House
@@ -25,14 +30,14 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.House
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
 
   }
 //   remove: function(req, res) {
-//     db.Book
+//     db.House
 //       .findById({ _id: req.params.id })
 //       .then(dbModel => dbModel.remove())
 //       .then(dbModel => res.json(dbModel))
