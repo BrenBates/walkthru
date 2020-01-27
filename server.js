f997cacfa479bcbcfa06850d50546c3089014ee9
Bren-lap <brennenbates2859@gmail.com>
2020-01-26 18:25:32 -0700

    adding heroku server info

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/walkthru");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds213529.mlab.com:13529/heroku_m9wjw5ll");
 
 // Start the API server
 app.listen(PORT, function() {
