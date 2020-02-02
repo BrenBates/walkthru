d8f389867bd5bc63936c663f2827491a30bc232e
colinSeven8 <ccnorse80@gmail.com>
2020-02-01 23:14:52 -0700

    styling landing page

--- client/src/pages/Navbar/Navbar.js
+++ client/src/pages/Navbar/Navbar.js
@@ -1,4 +1,5 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components'
 import { Link } from "react-router-dom";
 import "../Navbar/navbar.css";
 import {
@@ -12,60 +13,27 @@ import {
   NavbarText
 } from 'reactstrap';
 import { AuthContext } from "../../context/auth";
+
 const NavbarPage = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
     <div>
       <AuthContext.Consumer>
-      {authValue => (
-        <Navbar scrolling dark expand="md" fixed='top'>
-          <NavbarBrand className="brand" href="/">walkthru</NavbarBrand>
-          {/* {authValue.authTokens ? <h1>Authorized</h1>:<p></p>} */}
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              {/* <NavItem>
-             <NavLink tag={Link} to="/" className="text-info">Home</NavLink>
-              </NavItem> */}
-                {authValue.authTokens ? 
-                <>
-                  <NavItem>
-                  <NavLink tag={Link} to={`/api/user/${authValue.authTokens.username}`} className="text-info">Your Profile</NavLink> 
-                  </NavItem>
-                  <NavItem>
-                  <NavLink tag={Link} to="/Landing" className="text-info">Landing</NavLink>
-                  </NavItem>
-                  <NavItem>
-                  <NavLink tag={Link} onClick={props.logOut} to="/User" className="text-info">Log Out</NavLink>
-                  </NavItem>
-                </>
-                : ''}
-            </Nav>
-            <NavbarText />
-          </Collapse>
-        </Navbar>
-      )}
-      </AuthContext.Consumer>
-      
-    </div>
-  );
-}
-export default NavbarPage;
         {authValue => (
-          <Navbar dark expand="md" fixed='top'>
+          <Navbar scrolling dark expand="md" fixed='top'>
             <NavbarBrand className="brand" href="/">walkthru</NavbarBrand>
             {/* {authValue.authTokens ? <h1>Authorized</h1>:<p></p>} */}
             <NavbarToggler onClick={toggle} />
             <Collapse isOpen={isOpen} navbar>
               <Nav className="mr-auto" navbar>
+                {/* <NavItem>
+             <NavLink tag={Link} to="/" className="text-info">Home</NavLink>
+              </NavItem> */}
                 {authValue.authTokens ?
                   <>
                     <NavItem>
-                      <NavLink tag={Link} to="/" className="text-info">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink tag={Link} to="/User" className="text-info">Your Profile</NavLink>
+                      <NavLink tag={Link} to={`/api/user/${authValue.authTokens.username}`} className="text-info">Your Profile</NavLink>
                     </NavItem>
                     <NavItem>
                       <NavLink tag={Link} to="/Landing" className="text-info">Landing</NavLink>
@@ -81,6 +49,7 @@ export default NavbarPage;
           </Navbar>
         )}
       </AuthContext.Consumer>
+
     </div>
   );
 }
